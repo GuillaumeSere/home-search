9042071c2ed472513ffeb805511daed7110de7c3
Guillaume Sere <guillaumesere@hotmail.fr>
2022-08-24 15:46:53 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    images:{
+      domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+  },
+  }
